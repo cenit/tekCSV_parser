784eb8f259d151cbda0d6b84ac8a8500304f4fb8
Stefano Sinigardi <stesinigardi@hotmail.com>
2020-01-14 18:18:42 +0100

    fix reading

--- src/tekplot.py
+++ src/tekplot.py
@@ -7,6 +7,7 @@ import csv
 from pint import UnitRegistry, Quantity
 import argparse
 import numpy as np
+import pandas as pd
 import warnings
 
 with warnings.catch_warnings():
@@ -19,10 +20,6 @@ Q_ = u.Quantity
 
 
 class tekdata():
-    dt = [[], []]
-    data_x = [[], []]
-    data_y = [[], []]
-
     def __init__(self, inputFileName, outputFileName, showPlot=False):
         self.inputFileName = inputFileName
         self.outputFileName = outputFileName
@@ -34,29 +31,19 @@ class tekdata():
         """
         filename = self.inputFileName
         info = {}
-        x_mag, y_mag = [], []
+        x_mag, y1_mag, y2_mag = [], [], []
         with open(filename, 'rt', newline='') as f:
             reader = csv.reader(f, delimiter=',')
-            for row in reader:
-                if len(row) != 5: continue
-                key, value, _, x, y = row[:5]
+            model_key, model_value = next(reader)
+            firmware_key, firmware_value = next(reader)
+            _ = next(reader)  # skip empty line
+            print(
+                f'Reading {model_value} model file, firmware version {firmware_value}')
+
+            for x in range(0, 12):
+                key, value, _, _, _ = next(reader)
                 if key:
                     info[key] = value
-
-                try:
-                    x1 = float(x)
-                except ValueError:
-                    #print("Not a float: ", x)
-                    x1 = 0
-                try:
-                    y1 = float(y)
-                except ValueError:
-                    #print("Not a float: ", y)
-                    y1 = 0
-
-                x_mag.append(x1)
-                y_mag.append(y1)
-
             x_units = u.parse_units(info['Horizontal Units'])
             y_units = u.parse_units(info['Vertical Units'])
             x_offset = 0.  # Not in CSV?
@@ -64,20 +51,69 @@ class tekdata():
             x_scale = float(info['Horizontal Scale'])
             y_scale = float(info['Vertical Scale'])
 
-            x_mag_arr = np.array(x_mag)
-            y_mag_arr = np.array(y_mag)
+            print(
+                f'Horizontal Units: {x_units}, Vertical Units: {y_units}')
+            print(
+                f'Horizontal Offset: {x_offset}, Vertical Offset: {y_offset}')
+            print(
+                f'Horizontal Scale: {x_scale}, Vertical Scale: {y_scale}')
 
-            self.data_x = Q_((x_mag_arr - x_offset)*x_scale, x_units)
-            self.data_y = Q_((y_mag_arr - y_offset)*y_scale, y_units)
+            self.x_axis_label, self.y1_axis_label, _, self.y2_axis_label, _ = next(
+                reader)
+
+            for row in reader:
+                if len(row) != 5:
+                    continue
+                x, y1, _, y2, _ = row[:5]
+
+                try:
+                    xf = float(x)
+                except ValueError:
+                    # print("Not a float: ", x)
+                    xf = 0
+                try:
+                    y1f = float(y1)
+                except ValueError:
+                    # print("Not a float: ", y1)
+                    y1f = 0
+                try:
+                    y2f = float(y2)
+                except ValueError:
+                    # print("Not a float: ", y2)
+                    y2f = 0
+
+                x_mag.append(xf)
+                y1_mag.append(y1f)
+                y2_mag.append(y2f)
+
+            self.x_mag_arr = np.array(x_mag)
+            self.y1_mag_arr = np.array(y1_mag)
+            self.y2_mag_arr = np.array(y2_mag)
+
+            #hard_fixes
+            x_scale = 1
+            y_scale = 1
+
+            self.data_x = Q_((self.x_mag_arr - x_offset)*x_scale, x_units)
+            self.data_y1 = Q_((self.y1_mag_arr - y_offset)*y_scale, y_units)
+            self.data_y2 = Q_((self.y2_mag_arr - y_offset)*y_scale, y_units)
+
+            print(self.data_x[0])
+            print(self.data_x[1])
 
     def save_csv(self):
-        print("save_csv: Not implemented")
+        df = pd.DataFrame({self.x_axis_label: self.data_x, self.y1_axis_label: self.data_y1, self.y2_axis_label: self.data_y2})
+        df.to_csv(self.outputFileName, index=False, sep='\t', float_format='%3.5f', columns=[self.x_axis_label, self.y1_axis_label, self.y2_axis_label])
 
     def show_plots(self):
-        print("show_plots: Not implemented")
+        plt.plot(self.data_x, self.data_y1, label=self.y1_axis_label)
+        plt.plot(self.data_x, self.data_y2, label=self.y2_axis_label)
+        plt.legend(loc='upper left')
+        plt.show()
 
     def run(self):
         self.load_csv()
+        print(self.data_x, self.data_y1, self.data_y2)
         self.save_csv()
         if self.showPlot:
             self.show_plots()
